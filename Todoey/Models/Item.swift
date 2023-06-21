753219f0fe7fc0d49df38679ce994778fbbc82ea
Charizard17 <3723esad@gmail.com>
2023-06-21 20:50:13 +0200

    item model created and implemented

---
+++ Todoey/Models/Item.swift
@@ -0,0 +1,14 @@
+//
+//  TodoItemModel.swift
+//  Todoey
+//
+//  Created by Esad Dursun on 21.06.23.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
